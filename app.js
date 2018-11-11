479b640d1dd40f5b4d1a44c2b7172358d8cb687c
Jürgen Diez <>
2018-11-11 18:55:08 +0100

    Fixed vcontrol client instantiation for multiple concurrent jobs

--- app.js
+++ app.js
@@ -10,21 +10,22 @@ async function main() {
   const jobs = new Jobs("./jobs.json")
   const schema = jobs.getInfluxSchema()
 
-  let vControlClient = new VControlClient({
-    host: Config.vcontrold.host,
-    port: Config.vcontrold.port
-  })
   let influxClient = new Influx.InfluxDB({
     host: Config.influx.host,
     database: Config.influx.database,
     schema: schema
   })
 
-  let dataCollector = new DataCollector(vControlClient)
 
   jobs.jobs.forEach((job) => {
     setInterval(async () => {
       try {
+        let vControlClient = new VControlClient({
+          host: Config.vcontrold.host,
+          port: Config.vcontrold.port
+        })
+        let dataCollector = new DataCollector(vControlClient)
+
         let fields = await dataCollector.fetchData(job.fields)
         await influxClient.writePoints([{
           measurement: job.measurement,
